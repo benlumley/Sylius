bc6ad3a69b97a9fa37c75575bc3e465d9a6b7d9a
Ben Lumley <ben@benlumley.co.uk>
2019-03-01 16:53:48 +0000

    Fix compat

--- src/Sylius/Bundle/UserBundle/Form/Type/CustomerType.php
+++ src/Sylius/Bundle/UserBundle/Form/Type/CustomerType.php
@@ -39,7 +39,7 @@ class CustomerType extends CustomerProfileType
     /**
      * {@inheritdoc}
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
+    public function buildForm(FormBuilderInterface $builder, array $options = [])
     {
         parent::buildForm($builder, $options);
         $builder
